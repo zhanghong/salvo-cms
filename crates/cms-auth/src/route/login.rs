7d285d88c5ab4726019ad89595242243c8ce7225
zhanghong <zhanghongbean@gmail.com>
2025-02-22 11:28:42 +0800

    添加用户端登录接口

--- crates/cms-auth/src/route/login.rs
+++ crates/cms-auth/src/route/login.rs
@@ -96,3 +96,85 @@ pub async fn manager_delete(depot: &mut Depot) -> AppResult<()> {
     LoginService::delete(claims, state).await?;
     result_ok(())
 }
+
+/// 密码登录
+///
+/// 用户端密码登录
+#[endpoint(
+    tags("权鉴模块/用户端/登录"),
+    responses(
+        (status_code = 200, description = "success response")
+    )
+  )]
+pub async fn open_create(
+    depot: &mut Depot,
+    req: &mut Request,
+    json: JsonBody<LoginByPasswordForm>,
+) -> AppResult<LoginTokenCreateVO> {
+    let form = json.into_inner();
+    form.validate()?;
+    let state = depot.obtain::<AppState>().unwrap();
+    let mut dto: LoginStoreDTO = form.into();
+
+    // 获取 User-Agent
+    dto.user_agent = req
+        .headers()
+        .get(USER_AGENT)
+        .and_then(|v| v.to_str().ok())
+        .unwrap_or("")
+        .to_string();
+
+    // 获取客户端 IP（支持代理场景）
+    dto.client_ip = req.remote_addr().as_ipv4().unwrap().ip().to_string();
+
+    let token = LoginService::store(&PlatformEnum::Open, &dto, state).await?;
+    result_ok(token)
+}
+
+/// 刷新 AccessToken
+///
+/// 用户端刷新 AccessToken
+#[endpoint(
+      tags("权鉴模块/用户端/登录"),
+      responses(
+          (status_code = 200, description = "success response")
+      )
+    )]
+pub async fn open_update(depot: &mut Depot) -> AppResult<LoginTokenUpdateVO> {
+    let state = depot.obtain::<AppState>().unwrap();
+    let claims: Option<JwtClaimsDTO> = match depot.jwt_auth_state() {
+        JwtAuthState::Authorized => {
+            let data = depot.jwt_auth_data::<JwtClaimsDTO>().unwrap();
+            let claims = data.claims.clone();
+            Some(claims)
+        }
+        _ => None,
+    };
+
+    let vo = LoginService::update(claims, state).await?;
+    result_ok(vo)
+}
+
+/// 退出登录
+///
+/// 用户端删除 AccessToken
+#[endpoint(
+      tags("权鉴模块/用户端/登录"),
+      responses(
+          (status_code = 200, description = "success response")
+      )
+    )]
+pub async fn open_delete(depot: &mut Depot) -> AppResult<()> {
+    let state = depot.obtain::<AppState>().unwrap();
+    let claims: Option<JwtClaimsDTO> = match depot.jwt_auth_state() {
+        JwtAuthState::Authorized => {
+            let data = depot.jwt_auth_data::<JwtClaimsDTO>().unwrap();
+            let claims = data.claims.clone();
+            Some(claims)
+        }
+        _ => None,
+    };
+
+    LoginService::delete(claims, state).await?;
+    result_ok(())
+}
--- crates/cms-auth/src/route/mod.rs
+++ crates/cms-auth/src/route/mod.rs
@@ -10,15 +10,20 @@ pub fn init_router() -> Router {
         .push(Router::with_path("/checker/health").get(checker::health))
         .push(Router::with_path("/checker/database").get(checker::database))
         .push(Router::with_path("/manage/login/password").post(login::manager_create))
+        .push(Router::with_path("/manage/open/password").post(login::open_create))
         .push(
             Router::with_hoop(jwt_verify_refresh)
-                .push(Router::with_path("/manage/login").patch(login::manager_update)),
+                .push(Router::with_path("/manage/login").patch(login::manager_update))
+                .push(Router::with_path("/open/login").patch(login::open_update)),
         )
         .push(
-            Router::with_path("manage").hoop(jwt_verify_access).push(
-                Router::with_path("/login")
-                    .patch(login::manager_update)
-                    .delete(login::manager_delete),
-            ),
+            Router::with_path("manage")
+                .hoop(jwt_verify_access)
+                .push(Router::with_path("/login").delete(login::manager_delete)),
+        )
+        .push(
+            Router::with_path("open")
+                .hoop(jwt_verify_access)
+                .push(Router::with_path("/login").delete(login::open_delete)),
         )
 }
