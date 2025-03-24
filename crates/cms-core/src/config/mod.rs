9c3f3ff89dca17e09b788cf3e55b343548eb23ca
henry zhang <hong.zhang@advyzon.com>
2025-03-24 12:11:12 +0800

    RabbitMQ config

--- crates/cms-core/src/config/mod.rs
+++ crates/cms-core/src/config/mod.rs
@@ -1,11 +1,13 @@
 mod database;
 mod jwt;
+mod rabbitmq;
 mod redis;
 mod state;
 mod web;
 
 pub use database::DbConfig;
 pub use jwt::JwtConfig;
+pub use rabbitmq::RabbitMQConfig;
 pub use redis::RedisConfig;
 pub use state::AppState;
 pub use web::WebConfig;
--- crates/cms-core/src/config/state.rs
+++ crates/cms-core/src/config/state.rs
@@ -3,21 +3,25 @@ use sea_orm::DatabaseConnection;
 
 use super::DbConfig;
 use super::RedisConfig;
+use super::RabbitMQConfig;
 
 #[derive(Clone, Debug)]
 pub struct AppState {
     pub db: DatabaseConnection,
     pub redis: Client,
+    pub rabbitmq: String,
 }
 
 impl AppState {
     pub async fn init() -> Self {
         let db_config = DbConfig::from_env().expect("Failed to load db config");
         let redis_config = RedisConfig::from_env().expect("Failed to load redis config");
+        let rabbitmq_config = RabbitMQConfig::from_env().expect("Failed to load rabbitmq config");
 
         let db = db_config.build_connection().await.unwrap();
         let redis = redis_config.build_client().await.unwrap();
+        let rabbitmq = rabbitmq_config.url();
 
-        Self { db, redis }
+        Self { db, redis, rabbitmq }
     }
 }
